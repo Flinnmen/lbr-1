6431ca642e073b726ff6123e30eaef2cce4317d8
Flinnmen <vidsflinn@gmail.com>
2022-06-14 18:34:10 +0200

    bruh

--- Assets/SceneManagerScript.cs
+++ Assets/SceneManagerScript.cs
@@ -4,26 +4,47 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneManagerScript : MonoBehaviour
+<<<<<<< Updated upstream
 {
     
     public void test()
     {
         SceneManager.LoadScene("scheikundeMain");
+=======
+{   
+    public LoaderScriptMM scriptLoad;
+
+    public void loadMainMenu()
+    {
+        scriptLoad.LoadAnimation("MainMenu");
+>>>>>>> Stashed changes
     }
 
     void loadMainMenu()
     {
+<<<<<<< Updated upstream
         
+=======
+        scriptLoad.LoadAnimation("levelselect");
+>>>>>>> Stashed changes
     }
 
     void loadLevelselect()
     {
+<<<<<<< Updated upstream
         SceneManager.LoadScene("levelselect");
+=======
+        scriptLoad.LoadAnimation("scheikundeMain");
+>>>>>>> Stashed changes
     }
 
     void loadLevel1()
     {
+<<<<<<< Updated upstream
         
+=======
+        scriptLoad.LoadAnimation("bossScheikunde");
+>>>>>>> Stashed changes
     }
 
     void loadLevel2()
